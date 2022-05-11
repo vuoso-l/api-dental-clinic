32d016bfbe9312c3b24f73f136ed8f7e4df25e8e
vuoso-l <vuosolucas@gmail.com>
2022-05-11 12:09:16 -0300

    feat: modified the patient and dentist creation

--- src/test/java/com/example/dentalClinicApi/service/impl/DentistShiftServiceTest.java
+++ src/test/java/com/example/dentalClinicApi/service/impl/DentistShiftServiceTest.java
@@ -1,12 +1,17 @@
 package com.example.dentalClinicApi.service.impl;
 
+import com.example.dentalClinicApi.DTO.DentistDTO;
 import com.example.dentalClinicApi.DTO.DentistShiftDTO;
+import com.example.dentalClinicApi.DTO.PatientDTO;
 import com.example.dentalClinicApi.entity.Address;
 import com.example.dentalClinicApi.entity.Dentist;
 import com.example.dentalClinicApi.entity.Patient;
 import com.example.dentalClinicApi.repository.IDentistRepository;
 import com.example.dentalClinicApi.repository.IPatientRepository;
+import com.example.dentalClinicApi.service.IDentistService;
 import com.example.dentalClinicApi.service.IDentistShiftService;
+import com.example.dentalClinicApi.service.IPatientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +30,16 @@ class DentistShiftServiceTest {
     @Autowired
     IDentistShiftService iDentistShiftService;
     @Autowired
-    IPatientRepository iPatientRepository;
+    IPatientService iPatientService;
     @Autowired
-    IDentistRepository iDentistRepository;
+    IDentistService iDentistService;
+    @Autowired
+    ObjectMapper mapper;
 
     DentistShiftDTO dentShift1 = new DentistShiftDTO();
     DentistShiftDTO dentShiftTest;
-    Patient pat;
-    Dentist dent;
+    PatientDTO pat;
+    DentistDTO dent;
     Collection<DentistShiftDTO> readAll;
 
     @BeforeEach
@@ -42,12 +49,12 @@ class DentistShiftServiceTest {
         Patient patient = new Patient("Gómez", "Juan", Math.random() + "juan@gamil.com", "30123456", LocalDate.of(2022, 1, 1), address);
         Dentist dentist = new Dentist("123456" + Math.random(), "María", "García");
 
-        pat = iPatientRepository.save(patient);
-        dent = iDentistRepository.save(dentist);
+        pat = iPatientService.create(mapPatientDTO(patient));
+        dent = iDentistService.create(mapDentistDTO(dentist));
 
         dentShift1.setDate(LocalDate.of(2022, 5, 22));
-        dentShift1.setPatient(pat);
-        dentShift1.setDentist(dent);
+        dentShift1.setPatient(mapPatientEntity(pat));
+        dentShift1.setDentist(mapDentistEntity(dent));
         dentShiftTest = iDentistShiftService.create(dentShift1);
     }
 
@@ -74,8 +81,8 @@ class DentistShiftServiceTest {
     void update() {
         DentistShiftDTO getCreate = iDentistShiftService.findOne(dentShiftTest.getId());
         getCreate.setDate(LocalDate.of(2023, 1, 1));
-        getCreate.setPatient(pat);
-        getCreate.setDentist(dent);
+        getCreate.setPatient(mapPatientEntity(pat));
+        getCreate.setDentist(mapDentistEntity(dent));
         DentistShiftDTO readUpdate = iDentistShiftService.update(getCreate, getCreate.getId());
         assertEquals(iDentistShiftService.findOne(readUpdate.getId()).getDate(), LocalDate.of(2023, 1, 1));
         assertNotEquals(readUpdate, getCreate);
@@ -86,5 +93,18 @@ class DentistShiftServiceTest {
         assertNotNull(iDentistShiftService.findOne(dentShiftTest.getId()));
         iDentistShiftService.delete(dentShiftTest.getId());
         assertThrows(Exception.class, () -> iDentistShiftService.findOne(dentShiftTest.getId()));
+    }
+
+    private DentistDTO mapDentistDTO(Dentist dentist) {
+        return mapper.convertValue(dentist, DentistDTO.class);
+    }
+    public Dentist mapDentistEntity(DentistDTO dentistDTO) {
+        return mapper.convertValue(dentistDTO, Dentist.class);
+    }
+    private PatientDTO mapPatientDTO(Patient patient) {
+        return mapper.convertValue(patient, PatientDTO.class);
+    }
+    public Patient mapPatientEntity(PatientDTO patientDTO) {
+        return mapper.convertValue(patientDTO, Patient.class);
     }
 }
